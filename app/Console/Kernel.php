bbf54481dbfb3aa7eeaa28d88d81b431b4a11be7
Kristopher Wilson <kristopherwilson@gmail.com>
2021-04-04 13:29:28 -0400

    Scan command schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\Scan;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,9 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-
-        })->everyFifteenMinutes();
+        $schedule->command(Scan::class)->everyFifteenMinutes();
     }
 
     /**
